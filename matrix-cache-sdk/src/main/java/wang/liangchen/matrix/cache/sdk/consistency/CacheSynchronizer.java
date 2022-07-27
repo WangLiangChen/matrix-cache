197cb56546389a18fbb60356ea71212b080281cb
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-07-27 13:59:10 +0800

    优化redis队列同步器

--- matrix-cache-sdk/src/main/java/wang/liangchen/matrix/cache/sdk/consistency/CacheSynchronizer.java
+++ matrix-cache-sdk/src/main/java/wang/liangchen/matrix/cache/sdk/consistency/CacheSynchronizer.java
@@ -60,7 +60,9 @@ public enum CacheSynchronizer {
     }
 
     public void sendMessage(String name, String key) {
-        logger.debug("send message,name:{}, key:{}", name, key);
+        // 切换队列
+        String evictQueueKey = switchQueue();
+        logger.debug("send message,evictQueueKey:{}, name:{}, key:{}", evictQueueKey, name, key);
         // 写入队列
         if (null == key) {
             evictQueue.rightPush(name);
@@ -82,53 +84,61 @@ public enum CacheSynchronizer {
         this.evictQueue = this.redisTemplate.boundListOps(evictQueueKey);
         this.offset = this.evictQueue.size();
         logger.debug("inited evictQueueKey:{}, offset:{}", evictQueueKey, offset);
+    }
 
+    private synchronized String switchQueue() {
+        // 当前年月日
+        String nowKey = LocalDateTime.now().format(EVICT_QUEUE_KEY_FORMATTER);
+        String evictQueueKey = this.evictQueue.getKey().toString();
+        // 无需切换队列
+        if (nowKey.equals(evictQueueKey)) {
+            logger.debug("skip switch queue from:{} to:{}", evictQueueKey, nowKey);
+            return evictQueueKey;
+        }
+        // 切换队列
+        logger.debug("switch queue from:{} to:{}", evictQueueKey, nowKey);
+        this.previousEvictQueue = this.evictQueue;
+        this.previousOffset = this.offset;
+        evictQueueKey = nowKey;
+        this.evictQueue = this.redisTemplate.boundListOps(evictQueueKey);
+        this.offset = 0L;
+        return evictQueueKey;
     }
 
     private synchronized void pullMessage() {
-        String dateKey = LocalDateTime.now().format(EVICT_QUEUE_KEY_FORMATTER);
-        String evictQueueKey = this.evictQueue.getKey().toString();
-        logger.debug("evictQueueKey:{}, dateKey:{}", evictQueueKey, dateKey);
-        // 切换队列
-        if (!dateKey.equals(evictQueueKey)) {
-            logger.debug("switch queue from:{} to:{}", evictQueueKey, dateKey);
-            this.previousEvictQueue = this.evictQueue;
-            this.previousOffset = this.offset;
-            evictQueueKey = dateKey;
-            this.evictQueue = this.redisTemplate.boundListOps(evictQueueKey);
-            this.offset = 0L;
-        }
-        // 继续拉取上一个队列
-        if (null != this.previousEvictQueue) {
-            String previousEvictQueueKey = this.previousEvictQueue.getKey().toString();
-            Long size = this.previousEvictQueue.size();
-            logger.debug("pull from previousEvictQueue, key:{}, offset:{}, size:{}", previousEvictQueueKey, this.previousOffset, size);
-            if (size > this.previousOffset) {
-                List<Object> keys = this.previousEvictQueue.range(previousOffset, size - 1);
-                logger.debug("pulled from previousEvictQueue, key:{}, messages:{}", previousEvictQueueKey, keys.toString());
-                multilevelCacheManager.handleEvictedKeys(keys);
-                previousOffset = size;
-            }
-            // 拉取延时20S后,删除上一个队列
-            LocalDateTime zero = LocalDate.parse(evictQueueKey, EVICT_QUEUE_KEY_FORMATTER).atStartOfDay();
-            zero = zero.plusSeconds(20);
-            if (LocalDateTime.now().isAfter(zero)) {
-                logger.debug("delete and reset previousEvictQueue, key:{}", previousEvictQueueKey);
-                this.redisTemplate.delete(previousEvictQueueKey);
-                this.previousEvictQueue = null;
-                this.previousOffset = null;
-            }
-        }
+        String evictQueueKey = switchQueue();
         // 拉取当前队列
         Long size = this.evictQueue.size();
         logger.debug("pull from evictQueue, key:{}, offset:{}, size:{}", evictQueueKey, this.offset, size);
         if (size > this.offset) {
-            List<Object> keys = this.evictQueue.range(offset, size - 1);
-            logger.debug("pulled from evictQueue, key:{}, messages:{}", evictQueueKey, keys.toString());
-            multilevelCacheManager.handleEvictedKeys(keys);
+            List<Object> messages = this.evictQueue.range(offset, size - 1);
+            logger.debug("pulled from evictQueue, key:{}, messages:{}", evictQueueKey, messages.toString());
+            multilevelCacheManager.handleEvictedKeys(messages);
             this.offset = size;
         }
         pullTimestamp = System.currentTimeMillis();
+        // 上一个队列已被清理
+        if (null == this.previousEvictQueue) {
+            return;
+        }
+        // 上一个队列有效，继续拉取,直到被清理
+        String previousEvictQueueKey = this.previousEvictQueue.getKey().toString();
+        size = this.previousEvictQueue.size();
+        logger.debug("pull from previousEvictQueue, key:{}, offset:{}, size:{}", previousEvictQueueKey, this.previousOffset, size);
+        if (size > this.previousOffset) {
+            List<Object> messages = this.previousEvictQueue.range(previousOffset, size - 1);
+            logger.debug("pulled from previousEvictQueue, key:{}, messages:{}", previousEvictQueueKey, messages.toString());
+            multilevelCacheManager.handleEvictedKeys(messages);
+            previousOffset = size;
+        }
+        // 延时拉取20S后,删除上一个队列
+        LocalDateTime zero = LocalDate.parse(evictQueueKey, EVICT_QUEUE_KEY_FORMATTER).atStartOfDay();
+        zero = zero.plusSeconds(20);
+        if (LocalDateTime.now().isAfter(zero)) {
+            logger.debug("delete and reset previousEvictQueue, key:{}", previousEvictQueueKey);
+            this.redisTemplate.delete(previousEvictQueueKey);
+            this.previousEvictQueue = null;
+            this.previousOffset = null;
+        }
     }
-
 }
