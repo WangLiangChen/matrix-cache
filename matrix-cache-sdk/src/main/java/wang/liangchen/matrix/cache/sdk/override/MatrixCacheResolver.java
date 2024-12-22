9d4e2a9e24e7eb6c8e07ec89780209eb3801d4c0
Liangchen.Wang <sintrue@hotmail.com>
2024-12-22 13:20:04 +0800

    add parameter matrix.cache.ttl.random

--- matrix-cache-sdk/src/main/java/wang/liangchen/matrix/cache/sdk/override/MatrixCacheResolver.java
+++ matrix-cache-sdk/src/main/java/wang/liangchen/matrix/cache/sdk/override/MatrixCacheResolver.java
@@ -1,5 +1,7 @@
 package wang.liangchen.matrix.cache.sdk.override;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.interceptor.BasicOperation;
@@ -10,12 +12,15 @@ import wang.liangchen.matrix.cache.sdk.cache.MatrixCacheManager;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Liangchen.Wang 2022-07-04 13:43
  * find cache names from the contex, and find cache from the cachemanager
  */
 public class MatrixCacheResolver extends SimpleCacheResolver {
+    private static final Logger logger = LoggerFactory.getLogger(MatrixCacheResolver.class);
     private final CacheManager cacheManager;
 
     public MatrixCacheResolver(CacheManager cacheManager) {
@@ -25,18 +30,26 @@ public class MatrixCacheResolver extends SimpleCacheResolver {
 
     @Override
     public Collection<? extends Cache> resolveCaches(CacheOperationInvocationContext<?> context) {
-        Duration ttl = null;
-        if (cacheManager instanceof MatrixCacheManager) {
-            BasicOperation operation = context.getOperation();
-            if (operation instanceof MatrixCacheableOperation) {
-                ttl = ((MatrixCacheableOperation) operation).getTtl();
-            } else if (operation instanceof MatrixCachePutOperation) {
-                ttl = ((MatrixCachePutOperation) operation).getTtl();
-            }
+        if (!(cacheManager instanceof MatrixCacheManager)) {
+            return super.resolveCaches(context);
         }
-
         Collection<String> cacheNames = getCacheNames(context);
+        if (null == cacheNames || cacheNames.isEmpty()) {
+            return Collections.emptyList();
+        }
         Collection<Cache> caches = new ArrayList<>(cacheNames.size());
+        Duration ttl = null;
+        BasicOperation operation = context.getOperation();
+        if (operation instanceof MatrixCacheableOperation) {
+            ttl = ((MatrixCacheableOperation) operation).getTtl();
+        } else if (operation instanceof MatrixCachePutOperation) {
+            ttl = ((MatrixCachePutOperation) operation).getTtl();
+        }
+        // if random
+        if ((null == ttl || Duration.ZERO == ttl) && Boolean.parseBoolean(System.getProperty("matrix.cache.ttl.random", "false"))) {
+            ttl = Duration.ofMinutes(ThreadLocalRandom.current().nextInt(10, 120));
+            logger.debug("The ttl use random: {} minutes", ttl);
+        }
         Cache cache;
         for (String cacheName : cacheNames) {
             if (null == ttl || Duration.ZERO == ttl) {
